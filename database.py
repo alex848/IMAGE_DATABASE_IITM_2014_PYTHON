287645655fe2b41ace2f78a503cce07d06c403d8
Nishant Singh <saysnishant@gmail.com>
2015-06-26 14:48:18 +0530

    Sort based on gender added|| Name added

--- database.py
+++ database.py
@@ -2,6 +2,10 @@ import urllib
 import os
 import cv2
 import time
+#monkey patching to avoid certification failure
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+from BeautifulSoup import BeautifulSoup
 #cur directory using os.getcwd()
 path= "IIT-DATABASE"
 cur_dir= os.getcwd()
@@ -19,7 +23,10 @@ for i in branches:
     link_format= 'http://photos.iitm.ac.in/byroll.php?roll='
     if not os.path.exists(path+'/'+branch) :
         os.makedirs(path+'/'+branch)
-
+    if not os.path.exists(path+'/'+"Girls") :
+        os.makedirs(path+'/'+"Girls")
+    if not os.path.exists(path+'/'+"Boys") :
+        os.makedirs(path+'/'+"Boys")
     for j in year_codes :
         
         os.chdir(cur_dir+"/"+path+"/"+branch)
@@ -35,7 +42,6 @@ for i in branches:
             if i>99:
                     roll_np= branch+j+'B'+str(i)
             filename= roll_np+format
-            print filename
             f=open(filename,'wb')
             studentid= link_format+roll_np
             f.write(urllib.urlopen(studentid).read())
@@ -45,9 +51,34 @@ for i in branches:
             if width==144 and height==180 :
                 os.remove(filename)
             else :
+                print filename
+                soup_url= "https://www.iitm.ac.in/students/sinfo/"+roll_np
+                pageFile = urllib.urlopen(soup_url)
+                pageHtml = pageFile.read()
+                pageFile.close()
+                soup = BeautifulSoup("".join(pageHtml))
+                menu1= soup.find("div", {"class":"block block-system no-title"})
+                menu2= menu1.find("strong")
+                name= menu2.text
+                menu1.findAll("td")
+                print "Name: {}".format(name)
+                para=menu1.text
+                #print para
+                a= para.find("Gender")
+                index_gender= a+ len("Gender")
+                gender= para[index_gender]
+                print "Gender: {}".format(gender) 
                 font = cv2.FONT_HERSHEY_SIMPLEX
                 cv2.putText(image,roll_np,(int(.24*width),int(0.92875*height)), font, 1,(0,0,0),2,cv2.CV_AA)
                 cv2.imwrite(filename,image)
+                if gender=='M':
+                    os.chdir(cur_dir+"/"+path+"/"+"Boys")  
+                    cv2.imwrite(filename,image)
+                    os.chdir(cur_dir+"/"+path+"/"+branch+'/'+j)
+                else :
+                    os.chdir(cur_dir+"/"+path+"/"+"Girls")  
+                    cv2.imwrite(filename,image)
+                    os.chdir(cur_dir+"/"+path+"/"+branch+'/'+j)
 #time.sleep(0.1)
 
 
